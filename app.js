e67c467f22944da7e9b8bb8a52623648d913912d
Lido Lee <Lido_Lee@asus.com>
2022-08-24 22:21:35 +0800

    deploy to heroku server.

--- app.js
+++ app.js
@@ -51,6 +51,6 @@ app.use((err, req, res, next) => {
   res.status(500).send("something went wrong");
 });
 
-app.listen(8080, () => {
-  console.log("Server running on port 8080.");
+app.listen(process.env.PORT || 8080, () => {
+  console.log("Server is running on port 3000.");
 });
