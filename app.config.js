587aff64933f1aaa8de7ae7583a93d69f1823276
Lido Lee <Lido_Lee@asus.com>
2022-08-25 23:05:35 +0800

    redirectURL

--- app.config.js
+++ app.config.js
@@ -1,3 +1,4 @@
 //fron-end home page
-exports.MyToDoWeb_URL = "https://my-todo-web.netlify.app"; //"http://localhost:3000";
+exports.MyToDoWeb_URL =
+  "https://63078e67f8aeae0fb779d849--vermillion-kataifi-302bf8.netlify.app"; //"http://localhost:3000";
 exports.herokuServer_URL = "https://desolate-coast-61655.herokuapp.com"; //"http://localhost:3000";
