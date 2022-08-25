84adc8dae6bd78443bcffc9efbed44820512e3cc
Lido Lee <Lido_Lee@asus.com>
2022-08-25 17:19:47 +0800

    modify front-end url(link to netlify web).

--- app.config.js
+++ app.config.js
@@ -1,2 +1,2 @@
 //fron-end home page
-exports.MyToDoWeb_URL = "http://localhost:3000";
+exports.MyToDoWeb_URL = "https://my-todo-web.netlify.app/"; //"http://localhost:3000";
--- app.js
+++ app.js
@@ -52,5 +52,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(process.env.PORT || 8080, () => {
-  console.log("Server is running on port 3000.");
+  console.log("Server is running on port 8080.");
 });
